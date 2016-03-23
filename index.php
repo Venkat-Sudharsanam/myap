b0779851dfeee65b11bf9564f3a454bab816e5b3
Venkat M <admin@admin.com>
2016-03-23 06:26:21 +0000

    jenkins must do this change

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html>
  <body bgcolor="#00db04">
-  <h1> Version 3.0 <h1>
-  <h2> Sample PHP App <h2>
+  <h1> Version 5.0 <h1>
+  <h2> jenkins did it <h2>
   <h2> Deployed with Capistrano <h2>
     <?php
     echo "<h4>Current date and time => " . date("r") . "</h4>";
